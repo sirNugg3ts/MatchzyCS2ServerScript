90662fe01be992c646c4d4b70d8e43891297b204
Diogo Pascoal <diogopascoal789@gmail.com>
2023-12-16 19:09:38 +0000

    download cssharp separatly from matchzy
    Since matchzy comes with the linux build, I've implemented the cssharp windows build separatly

--- MatchzyServer.ps1
+++ MatchzyServer.ps1
@@ -12,7 +12,7 @@ process {
 
     }
     $Host.UI.RawUI.BackgroundColor = "Black"
-Clear-Host
+    Clear-Host
     # Check if SteamCMD is installed
     if (!(Test-Path "$steamcmdPath\steamcmd.exe")) {
         Write-Host "Error: SteamCMD not found. Please provide the correct path in the script." -BackgroundColor Red
@@ -64,26 +64,50 @@ Clear-Host
   
     # Define the URL of the latest release
     $API_URL = "https://api.github.com/repos/shobhit-pathak/MatchZy/releases/latest"
-  
+
+    #Define the URL of the latest cssharp release
+    $CSSHARP_API_URL = "https://api.github.com/repos/roflmuffin/CounterStrikeSharp/releases/latest"
+
+    $asset = Invoke-RestMethod -Uri $CSSHARP_API_URL | Select-Object -ExpandProperty assets | Select-Object -Index 3
+
+    # Send a GET request to the GitHub API and parse the JSON response to extract the download URL from the 4th asset
+    $CSSHARP_RELEASE_URL = $asset.browser_download_url  
     # Send a GET request to the GitHub API and parse the JSON response to extract the download URL
     $RELEASE_URL = (Invoke-RestMethod -Uri $API_URL | Select-Object -ExpandProperty assets | Select-Object -First 1 -ExpandProperty browser_download_url)
   
-    Write-Host "Downloading the latest release from GitHub..." -BackgroundColor Yellow -ForegroundColor Black
-    Write-Host $RELEASE_URL
-  
+    # Download the latest cssharp release and save it to the temporary directory
+
+    Write-Host "Downloading the CounterStrikeSharp latest release from GitHub..." -BackgroundColor Yellow -ForegroundColor Black
+    Write-Host $CSSHARP_RELEASE_URL
+
     # Define the path of the temporary directory
     $TEMP_DIR = "$env:TEMP\MatchZy"
-  
+
     # Create the temporary directory if it doesn't exist
     if (-not (Test-Path -Path $TEMP_DIR)) {
         New-Item -ItemType Directory -Path $TEMP_DIR | Out-Null
     }
-  
+
     # Delete all files in the temporary directory
     Get-ChildItem -Path $TEMP_DIR -Recurse | Remove-Item -Force -Recurse
+
+    
+  
   
     # Download the latest release and save it to the temporary directory
-    Invoke-WebRequest -Uri $RELEASE_URL -OutFile "$TEMP_DIR\MatchZy.zip"
+    Invoke-WebRequest -Uri $CSSHARP_RELEASE_URL -OutFile "$TEMP_DIR\CSSHarp.zip"
+
+    # Extract the downloaded file 
+    Expand-Archive -Path "$TEMP_DIR\CSSHarp.zip" -DestinationPath "$TEMP_DIR\extracted" -Force
+
+    Write-Host "Downloading the Matchzy latest release from GitHub..." -BackgroundColor Yellow -ForegroundColor Black
+    Write-Host $RELEASE_URL
+  
+
+    # Download the latest release and save it to the temporary directory
+    Invoke-WebRequest -Uri $RELEASE_URL -OutFile "$TEMP_DIR\Matchzy.zip"
+   
+  
   
     # Extract the downloaded file 
     Expand-Archive -Path "$TEMP_DIR\MatchZy.zip" -DestinationPath "$TEMP_DIR\extracted" -Force
